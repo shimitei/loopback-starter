078844d90c0559cd517dc4aa27f00c67099a396e
Satoshi Shimizu <shimitei@gmail.com>
2017-03-30 19:41:23 +0900

    Fix lint error

--- common/models/account.js
+++ common/models/account.js
@@ -1,23 +1,24 @@
 'use strict';
 
 module.exports = function(Account) {
-    // Disable related accessTokens API
-    Account.disableRemoteMethodByName('prototype.__count__accessTokens');
-    Account.disableRemoteMethodByName('prototype.__create__accessTokens');
-    Account.disableRemoteMethodByName('prototype.__delete__accessTokens');
-    Account.disableRemoteMethodByName('prototype.__destroyById__accessTokens');
-    Account.disableRemoteMethodByName('prototype.__findById__accessTokens');
-    Account.disableRemoteMethodByName('prototype.__get__accessTokens');
-    Account.disableRemoteMethodByName('prototype.__updateById__accessTokens');
+  // Disable related accessTokens API
+  Account.disableRemoteMethodByName('prototype.__count__accessTokens');
+  Account.disableRemoteMethodByName('prototype.__create__accessTokens');
+  Account.disableRemoteMethodByName('prototype.__delete__accessTokens');
+  Account.disableRemoteMethodByName('prototype.__destroyById__accessTokens');
+  Account.disableRemoteMethodByName('prototype.__findById__accessTokens');
+  Account.disableRemoteMethodByName('prototype.__get__accessTokens');
+  Account.disableRemoteMethodByName('prototype.__updateById__accessTokens');
 
-    // Remove existing validations for email
-    delete Account.validations.email;
+  // Remove existing validations for email
+  delete Account.validations.email;
 
-    const re_username = /^[a-zA-Z0-9_\-]+$/;
-    Account.validate('username',
-        function (err) {
-            if (this.username !== undefined && !re_username.test(this.username)) err();
-        },
-        { message: 'VALIDATE_ERR_ACCOUNT_USERNAME_PATTERN' });
-
+  const validUsername = /^[a-zA-Z0-9_\-]+$/;
+  Account.validate('username',
+    function(err) {
+      if (this.username !== undefined &&
+        !validUsername.test(this.username)) err();
+    },
+    {message: 'VALIDATE_ERR_ACCOUNT_USERNAME_PATTERN'}
+  );
 };
