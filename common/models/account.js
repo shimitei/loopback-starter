cd0cde3d28a55c7ac80cb797c3be059cb77b0d52
Satoshi Shimizu <shimitei@gmail.com>
2017-03-30 17:32:18 +0900

    Add login account model

---
+++ common/models/account.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function(Account) {
+    // Disable related accessTokens API
+    Account.disableRemoteMethodByName('prototype.__count__accessTokens');
+    Account.disableRemoteMethodByName('prototype.__create__accessTokens');
+    Account.disableRemoteMethodByName('prototype.__delete__accessTokens');
+    Account.disableRemoteMethodByName('prototype.__destroyById__accessTokens');
+    Account.disableRemoteMethodByName('prototype.__findById__accessTokens');
+    Account.disableRemoteMethodByName('prototype.__get__accessTokens');
+    Account.disableRemoteMethodByName('prototype.__updateById__accessTokens');
+
+    // Remove existing validations for email
+    delete Account.validations.email;
+
+    const re_username = /^[a-zA-Z0-9_\-]+$/;
+    Account.validate('username',
+        function (err) {
+            if (this.username !== undefined && !re_username.test(this.username)) err();
+        },
+        { message: 'VALIDATE_ERR_ACCOUNT_USERNAME_PATTERN' });
+
+};
